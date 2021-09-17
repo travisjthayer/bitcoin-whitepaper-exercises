9ab5b3ff97ad3643f0886c61bf516bb533b27232
Travis Thayer <travisthayer@traviss-mini-2.attlocal.net>
2021-09-17 08:35:16 -0400

    Completed hashing exercise

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -23,21 +23,77 @@ var Blockchain = {
 Blockchain.blocks.push({
 	index: 0,
 	hash: "000000",
+	prevHash: "000000",
 	data: "",
 	timestamp: Date.now(),
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	console.log("\n\nRetreiving Block Data...")
+	createBlock(line)
+}
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+// Create a function called createBlock()
+function createBlock(_data) {
+	let block = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+		data: _data,
+		timestamp: Date.now()
+	}
+	block.hash = blockHash(block)
 
+	// verify block before adding to chain
+	console.log("Verifying block...")
+	console.log("Block Details: ", block)
+	if(verifyBlock(block)) {
+
+		console.log(`Block is valid: ${verifyBlock(block)}`)
+		console.log("\nAdding Block...")
+
+		// once verified add to the chain
+		Blockchain.blocks.push(block)
+		console.log("Block added to Blockchain")
+
+		// once block added confirm blockchain is valid
+		console.log("\nVerifying Blockchain....")
+		if(verifyChain(block.index)) {
+			console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+		} else {
+			// error handling
+		}
+
+	} else {
+		// error handling
+		console.log(`Block is valid: ${verifyBlock(block)}`);
+		console.log(block)
+	}
+	return block
+}
 
 // **********************************
 
 function blockHash(bl) {
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		`${bl.index};${bl.prevHash};${bl.data};${bl.timestamp};`
 	).digest("hex");
+}
+
+function verifyChain(_index) {
+
+	if (Blockchain.blocks[0]) {
+		return true
+	} else if (Blockchain.blocks[_index].prevHash == Blockchain.blocks[_index - 1].hash) {
+		return true
+	}
+}
+
+function verifyBlock(block) {
+	if(block.data != '' && block.prevHash != '' && block.index >= 0) {
+		if(block.hash == blockHash(block)) {
+			return true
+		}
+	}
 }
